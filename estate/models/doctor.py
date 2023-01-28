4a53a0f0c7190a4689d1f3c14f1be3c33cc6dd40
Carlos Lopez <celm1990@gmail.com>
2023-01-28 10:01:22 -0500

    [ADD] temas varios * onchange *constraints * compute * herencias

--- estate/models/doctor.py
+++ estate/models/doctor.py
@@ -5,6 +5,7 @@ class Doctor(models.Model):
     _name = 'doctor'
 
     name = fields.Char()
+    apellido = fields.Char()
     paciente_ids = fields.One2many(comodel_name="paciente", inverse_name="doctor_id")
 
     def unlink(self):
--- estate/models/pacientes.py
+++ estate/models/pacientes.py
@@ -1,12 +1,40 @@
-from odoo import models, fields
-from odoo.exceptions import UserError
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Paciente(models.Model):
     _name = 'paciente'
 
     name = fields.Char()
+    price_unit = fields.Float(string="Precio Unitario")
+    product_qty = fields.Float(strig="Cantidad")
+    subtotal = fields.Float(strig="Subtotal", compute="_compute_subtotal", store=True)
     doctor_id = fields.Many2one(comodel_name="doctor")
+    apellido_doctor = fields.Char()
     doctor_ids = fields.Many2many(comodel_name="doctor", string="Doctores")
+
+    _sql_constraints = [
+        (
+            "doctor_unique",
+            "unique(doctor_id)",
+            "El doctor no se puede repetir por paciente!",
+        )
+    ]
+
+    @api.constrains('price_unit')
+    def _check_price_unit(self):
+        for record in self:
+            if record.price_unit <0:
+                raise ValidationError(f"El precio unitario debe ser mayor a cero, del paciente: {record.name}")
+        
+    @api.onchange("doctor_id")
+    def _onchange_doctor_id(self):
+        self.apellido_doctor = self.doctor_id.apellido
+
+    @api.depends("price_unit", "product_qty")
+    def _compute_subtotal(self):
+        for record in self:
+            record.subtotal = record.price_unit * record.product_qty
+
 
     def crear_doctor(self):
         DoctorModel = self.env["doctor"]
